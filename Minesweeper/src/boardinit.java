b0bd1604974a64f42a4a33e2510e946fdb9e4be4
Collinnn <51782585+Collinnn@users.noreply.github.com>
2021-01-05 18:19:23 +0100

    Starup på Board og håndtering

---
+++ Minesweeper/src/boardinit.java
@@ -0,0 +1,43 @@
+
+public class boardinit {
+	private int collum, row,num_mines;
+	private int [][] grid; 
+	
+	public boardinit(int collum, int row,int num_mines) {
+		this.collum=collum;
+		this.row=row;
+		this.num_mines=num_mines;
+		this.grid= new int[collum][row];
+	}
+	
+	public static void pupulateboard() {
+		
+	}
+	
+	public int getCollum() {
+		return collum;
+	}
+	public int getrow() {
+		return row;
+	}
+	public int getmines() {
+		return num_mines;
+	}
+	
+	public int getNeighbors(int x, int y) {
+		int neighbor = 0;
+		neighbor = grid[x-1][y-1]==1 ? neighbor++ : neighbor;
+		neighbor = grid[x-1][y]==1 ? neighbor++ : neighbor;
+		neighbor = grid[x-1][y+1]==1 ? neighbor++ : neighbor;
+		neighbor = grid[x][y-1]==1 ? neighbor++ : neighbor;
+		neighbor = grid[x][y+1]==1 ? neighbor++ : neighbor;
+		neighbor = grid[x+1][y-1]==1 ? neighbor++ : neighbor;
+		neighbor = grid[x+1][y]==1 ? neighbor++ : neighbor;
+		neighbor = grid[x+1][y+1]==1 ? neighbor++ : neighbor;
+		return neighbor;
+	}
+	
+	
+	
+}
+

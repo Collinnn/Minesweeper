2acf7e3165759dc4b86287feb6b4728fd7d528b6
Collinnn <51782585+Collinnn@users.noreply.github.com>
2021-01-11 13:31:25 +0100

    Timer class, alt ui skal stadig skrives.

---
+++ Minesweeper/src/timer.java
@@ -0,0 +1,34 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+
+
+public class timer {
+	public Timeline timeline;
+	
+	public timer() {
+		
+		
+		timeline = new Timeline();
+		timeline.setCycleCount(Timeline.INDEFINITE);
+		timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1)));	
+	}
+	
+	
+	public void stopcounter(boolean stop) {
+		if(stop) {
+			timeline.pause();
+		}
+		else {
+			timeline.play();
+		}
+	}
+	public void restartcounter() {
+		timeline.playFromStart();
+	}
+	public int getTime() { 
+		return timeline.getCycleCount();
+	}
+	
+	
+}

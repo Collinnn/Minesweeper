716efc20b8091ddb3da38cba0e8d432368532fb0
Agentii <Agentii@users.noreply.github.com>
2021-01-13 11:54:41 +0100

    Added generateGroup() & get_3BV()
    Includes method to generate the group the specified tile belongs to and method to get the 3BV score

--- Minesweeper/src/BV3.java
+++ Minesweeper/src/BV3.java
@@ -1,8 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 
 
-	 
+public class BV3 {
+	
+	public static HashSet<Tile> generateGroup(Tile tile) {
+		HashSet<Tile> group = new HashSet<Tile>();
+		group.add(tile);
+		if (tile.get_value() == 0 && !Tile.bombTiles.contains(tile)) {
+			tile.clicked = true;
+			for (Tile neighbor : tile.get_neighbors()) {
+				if (!group.contains(neighbor) && !neighbor.clicked) {
+					neighbor.clicked = true;
+					group.addAll(generateGroup(neighbor));
+				}
+			}
+		}
+		else {
+			for (Tile neighbor : tile.get_neighbors()) {
+				if (!tile.clicked && neighbor.get_value() == 0 && !Tile.bombTiles.contains(neighbor)) {
+					group.addAll(generateGroup(neighbor));
+				}
+			}
+		}
+		tile.clicked = false;
+		
+		return group;
+	}
 	
 	
 	
-	
-	
+	public static Integer get_3BV() {
+		HashSet<HashSet<Tile>> groups = new HashSet<HashSet<Tile>>();
+		for (int row = 0; row < Main.HEIGHT; row++) {
+			for (int col = 0; col < Main.WIDTH; col++) {
+				Tile tile = Tile.tiles[row][col];
+				if (!Tile.bombTiles.contains(tile)) {
+					HashSet<Tile> tileGroup = generateGroup(tile);
+					groups.add(tileGroup);
+				}
+			}
+		}
+		return groups.size();
+	}
+}

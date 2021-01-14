db2b13039390cd3fa11bf723f2ae88b01afe9e58
Collinnn <51782585+Collinnn@users.noreply.github.com>
2021-01-14 15:44:58 +0100

    Hints update

--- Minesweeper/src/hints.java
+++ Minesweeper/src/hints.java
@@ -1,13 +1,14 @@
-import java.util.ArrayList;
-
+import javafx.scene.paint.Color;
+import java.util.ArrayList; 
 public class hints {
 	
 	public ArrayList<Tile> GenerateBitmap() {
 		ArrayList<Tile> bitmap = new ArrayList<Tile>();
-		for (int row = 0; row < Main.HEIGHT;row++) {
-			for (int col = 0; col <Main.WIDTH;col++) {
-				if(Tile.clicked) {
-					bitmap.add(Tile.tiles[row][col]);
+		for (int row = 0; row < Board.HEIGHT;row++) {
+			for (int col = 0; col <Board.WIDTH;col++) {
+				Tile tile = Board.tiles[row][col];
+				if(tile.clicked) {
+					bitmap.add(tile);
 				}
 				else {
 					bitmap.add(null);
@@ -18,40 +19,46 @@ public class hints {
 	}
 	
 	public void gethint () {
-		int n;
-		for (int row = 0; row < Main.HEIGHT;row++) {
-			for (int col = 0; col <Main.WIDTH;col++) {
-				if(Tile.clicked && Tile.get_value()==getNeighbors(row,col)) {   
+		for (Tile[] tilerow: Board.tiles) {
+			for (Tile tile: tilerow) {
+				if(tile.clicked && tile.get_value()==Board.get_neighbors(tile).size()) {   
+					for(Tile neighbor: findNeighbors(tile)) {
+						neighbor.set_highlight(Color.GOLD);
+					}
 					
 				}
 			}
 		}
 	}
+
+	private ArrayList<Tile> findNeighbors(Tile tile) {
+		ArrayList<Tile> notclick = new ArrayList<Tile>();
+		for(Tile neighbor: Board.get_neighbors(tile)) {
+			if(!neighbor.clicked) {
+				notclick.add(neighbor); 
+			}
+		}
+		
+		return notclick; 
+	}
 	
+	
+	
+	/*
+	  	
 	private int getNeighbors(int row, int col) {
 		ArrayList<Tile> neighbors = new ArrayList<Tile>();
 		int n=0; 
-		for (int i = -1; i <= 1; i++) {
-			for (int j = -1; j <= 1; j++) {
-				if(!Tile.clicked) {
-					n++; 
-				}
+		for(Tile neighbor : Board.get_neighbors(this)) {
+			
+				
 			}
 		
 		}
-		return n; 
+		return null; 
 		 
 	}
-	private ArrayList<Tile> findNeighbors(int row, int col) {
-		for (Tile tile : Tile.get_neighbors()) {
-			array
-		}
-		return null;
-	}
-	
-	
-	
-	
+	 */
 	
 	
 	

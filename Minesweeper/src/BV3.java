4f31745fc503d6a64bdc04c7c10a070cef7a0760
Agentii <Agentii@users.noreply.github.com>
2021-01-14 16:13:35 +0100

    Moved generateGroup method to board

--- Minesweeper/src/BV3.java
+++ Minesweeper/src/BV3.java
@@ -3,32 +3,6 @@ import java.util.HashSet;
 
 public class BV3 {
 	
-	public static HashSet<Tile> generateGroup(Tile tile) {
-		HashSet<Tile> group = new HashSet<Tile>();
-		group.add(tile);
-		if (tile.get_value() == 0 && !Board.bombTiles.contains(tile)) {
-			tile.clicked = true;
-			for (Tile neighbor : Board.get_neighbors(tile)) {
-				if (!group.contains(neighbor) && !neighbor.clicked) {
-					neighbor.clicked = true;
-					group.addAll(generateGroup(neighbor));
-				}
-			}
-		}
-		else {
-			for (Tile neighbor : Board.get_neighbors(tile)) {
-				if (!tile.clicked && neighbor.get_value() == 0 && !Board.bombTiles.contains(neighbor)) {
-					group.addAll(generateGroup(neighbor));
-				}
-			}
-		}
-		tile.clicked = false;
-		
-		return group;
-	}
-	
-	
-	
 	public static Integer get_3BV() {
 		HashSet<HashSet<Tile>> groups = new HashSet<HashSet<Tile>>();
 		for (int row = 0; row < Board.HEIGHT; row++) {

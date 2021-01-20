9d266f690429394737ea54d39d5bb453dc53ff5f
SimonSundt <s204445@student.dtu.dk>
2021-01-20 14:28:50 +0100

    Temp data-file changes

--- Minesweeper/src/HighscoresWindow.java
+++ Minesweeper/src/HighscoresWindow.java
@@ -81,11 +81,11 @@ public class HighscoresWindow {
 		public static void listViewFill(ListView<String> listview, File f) throws FileNotFoundException {
 			Highscores.read(f);
 			for(int i = 0; i < Highscores.getHighscores().length; i++) {
-				if(Highscores.getHighscores()[i].equals("-1:***")) {
+				if(Highscores.getHighscores()[i].equals("500:***")) {
 					listview.getItems().add("-\t\t-");
 				} else {
-					listview.getItems().add(Highscores.getHighscores()[i].split(":")[0] + "\t\t" + 
-				Highscores.getHighscores()[i].split(":")[1]);
+					listview.getItems().add(Highscores.getHighscores()[i].split(":")[1] + "\t\t" + 
+				Highscores.getHighscores()[i].split(":")[0]);
 				}
 			}
 		}

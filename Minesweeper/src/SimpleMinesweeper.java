4f0c190ee3669ff93c3130cdcf1006ef33013fec
Agentii <Agentii@users.noreply.github.com>
2021-01-21 13:13:15 +0100

    Simple minesweeper update

--- Minesweeper/src/SimpleMinesweeper.java
+++ Minesweeper/src/SimpleMinesweeper.java
@@ -15,24 +15,19 @@ import java.util.ArrayList;
  */
 
 public class SimpleMinesweeper extends Application {
-	// JavaFX button object used for the tiles
 	Button button;
 	
-	static int tileSize = 40; // width and height of tiles/button object
-	static int rowSize = 15; // Number of tiles in a row
-	static int colSize = 10; // Number of tiles in a column
-	static int NoOfBombs = 25; // Number of bombs
+	static int tileSize = 40; // Width and height of tiles
+	static int rowSize = 15; 
+	static int colSize = 10; 
+	static int NoOfBombs = 25;
 	
-	// Button array/matrix: Stores all tiles
-	static Button[][] tiles = new Button[rowSize][colSize];
-	
-	// Button array list: Stores all tiles containing a bomb/mine
+	// Arrays stores all tiles and tiles containing bombs
+	static Button[][] tiles;
 	static ArrayList<Button> bombTiles = new ArrayList<Button>();
 	
-	// Loads bomb image
 	static Image img = new Image("bomb1.png");
 	
-	// GridPane object for storing the tiles in a grid-layout
 	static GridPane grid = new GridPane();
 	
 	public static void main(String[] args) {
@@ -40,25 +35,28 @@ public class SimpleMinesweeper extends Application {
 			rowSize = Integer.valueOf(args[0]);
 			colSize = Integer.valueOf(args[1]);
 			NoOfBombs = Integer.valueOf(args[2]);
+			
+			if (rowSize < 4 || colSize < 4 || rowSize > 100 || colSize > 100) {
+				throw new Exception("Invalid argument(s):\n\t-The grid size must be between 4x4 and 100x100");
+			}
 		}
-		catch(Exception e) {
+		catch(Exception exception) {
+			System.out.println(exception);
+			System.exit(0);
 		}
 		
-		// initializes the board with mines and sets 
-		// an action to be performed when clicking a tile
+		// initializes the board
+		tiles = new Button[rowSize][colSize];
 		init_tiles();
 		init_bombs();
 		set_tileAction();
 		
-		// Launches the main game loop
 		launch(args);
 	}
 	
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("Minesweeper");
-		
-		// Creates a scene in the main window with the grid layout
 		stage.setScene(new Scene(grid));
 	    
 	    stage.show();
@@ -66,15 +64,14 @@ public class SimpleMinesweeper extends Application {
 	
 	public static void init_tiles() {
 		/*
-		 * Initializes the board by adding tiles
-		 * to the grid layout
+		 * Initializes the board by adding tiles to the grid layout
 		 */
 		for (int row = 0; row < rowSize; row++) {
 	        for (int col = 0; col < colSize; col++) {
 	            Button button = new Button();
-	            button.setPrefSize(tileSize, tileSize); //sets the size of the tile
-	            grid.add(button, row, col); // adds the tile to the grid layout at the given row and column
-            	tiles[row][col] = button; // Stores the tile in the tile array at the given row and column
+	            button.setPrefSize(tileSize, tileSize); 
+	            grid.add(button, row, col); 
+            	tiles[row][col] = button; 
 	        }
 	    }
 	}
@@ -85,28 +82,23 @@ public class SimpleMinesweeper extends Application {
 		 */
 		
 		Random rand = new Random();
-		// Bomb counter: Counts number of bombs added to the game
 		int bombs = 0; 
 	    
 		while (bombs < NoOfBombs) {
-			
-			// Selects a random row and column
+			// Selects a random tile
 	    	int randRow = rand.nextInt(rowSize);
 	    	int randCol = rand.nextInt(colSize);
+	       	Button tile = tiles[randRow][randCol];
 	    	
-	    	// Assigns the tile at the random row and column to a variable
-	    	Button tile = tiles[randRow][randCol];
-	    	
-	    	// Adds a bomb to the assigned tile unless 
-	    	// the tile already contains a bomb
+	    	// Adds a bomb to the assigned tile unless the tile already contains a bomb
 	    	if (!bombTiles.contains(tile)) {
 	    		bombTiles.add(tile);
 	    		
-	    		// ImageView object: Image of bomb which will be shown when 
-	    		// clicking on a tile.
+	    		// Adds bomb image to tile
 	    		ImageView view = new ImageView(img);
 	            view.setFitWidth(20);
 	        	view.setPreserveRatio(true);
+	        	
 	        	// Sets the action of the button to show the bomb image
 	            tiles[randRow][randCol].setOnAction(e ->tiles[randRow][randCol].setGraphic(view));
 	           
@@ -121,30 +113,19 @@ public class SimpleMinesweeper extends Application {
 		 * The number will be shown when the tile i clicked.
 		 */
 		
-		// loops over every row and column in the tile grid
 		for (int row = 0; row < rowSize; row++) {
 	    	for (int col = 0; col < colSize; col++) {
-	    		
-	    		// Variable stores number of bombs next to a tile
 	    		int neighborBombs = 0; 
-	    		
-	    		// loops over every tile lying next to the tile at the specified row and column
 	    		for (Button tile : get_neighbors(row, col)) {
 		    		if (bombTiles.contains(tile)) {
-		    			// increments the variable if a neighboring tile is a bomb
 		    			neighborBombs++;
 		    		}
 		    	}
 	    		
-	    		// Assigns the tile/button at the specified row and column to a variable
 	    		Button tile = tiles[row][col];
 	    		
+	    		// Applies number/value to all tiles except bombs and blank tiles
 	    		if (!bombTiles.contains(tile) && neighborBombs != 0) {
-	    			/*
-	    			 * If the tile is not a bomb and at least one of the neighboring 
-	    			 * tiles has a bomb:
-	    			 * Set the tile's on-click action to display the number of neighboring bombs.
-	    			*/  
 		    		String finalNeighborBombs = Integer.toString(neighborBombs);
 		    		tiles[row][col].setOnAction(e -> tile.setText(finalNeighborBombs));
 	    		}
@@ -154,7 +135,6 @@ public class SimpleMinesweeper extends Application {
 	
 	public static ArrayList<Button> get_neighbors(int row, int col) {
 		/*
-		 * Parameters: Row and column from the grid of tiles (to select a tile)
 		 * Returns: ArrayList containing the selected tile's surrounding 8 tiles
 		 */
 		

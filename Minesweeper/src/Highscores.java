aa4f5e7fd828d602df56a62718f1b2dfba053824
SimonSundt <s204445@student.dtu.dk>
2021-01-21 18:41:02 +0100

    removed unused import declaration

--- Minesweeper/src/Highscores.java
+++ Minesweeper/src/Highscores.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Highscores {
